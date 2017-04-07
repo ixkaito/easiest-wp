1088e2a67b099d9e7d37c94526eb02ee69e22e3f
Kite <ixkaito@gmail.com>
2017-04-07 13:07:10 +0900

    Add body_class

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 	<header class="page-header">
 		<div class="header-area">
 			<div class="panel-site-title">
