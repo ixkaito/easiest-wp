4bce97bba275cfa07a02b249510699110eff10b4
Kite <ixkaito@gmail.com>
2017-04-07 12:42:17 +0900

    <meta charset="<?php bloginfo( 'charset' ); ?>">

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Easiest WP</title>
 	<?php wp_head(); ?>
