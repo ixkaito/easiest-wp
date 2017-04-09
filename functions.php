7d7a0015878e06d2ae7f8805d865034933d4d05a
Kite <ixkaito@gmail.com>
2017-04-10 03:50:53 +0900

    add_theme_support( 'post-thumbnails' );

--- functions.php
+++ functions.php
@@ -6,5 +6,7 @@ add_action( 'wp_enqueue_scripts', 'easiestwp_scripts' );
 
 function easiestwp_setup() {
 	add_theme_support( 'title-tag' );
+
+	add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'easiestwp_setup' );
