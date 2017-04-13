e68a926661542e7562bf1806c33e101ae3d54820
Kite <ixkaito@gmail.com>
2017-04-14 07:23:58 +0900

    Add new lines

--- index.php
+++ index.php
@@ -54,11 +54,13 @@
 
 	<footer class="page-footer">
 		<div class="footer-widget-area">
+
 			<?php if ( is_active_sidebar( 'footer' ) ) : ?>
 				<ul class="footer-widgets">
 					<?php dynamic_sidebar( 'footer' ); ?>
 				</ul>
 			<?php endif; ?>
+
 			<div class="back-to-top">
 				<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
 			</div>
