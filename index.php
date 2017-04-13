0443c4621521511853ad5a7490cd41c32841c889
Kite <ixkaito@gmail.com>
2017-04-14 05:25:58 +0900

    Use wp_nav_menu()

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
 				<p class="site-subtitle"><?php bloginfo( 'description' ); ?></p>
 			</div>
 
-			<nav class="global-nav">
-				<ul id="global-menu" class="menu">
-					<li class="current-menu-item"><a href="index.html">ホーム</a></li>
-					<li><a href="portfolio.html">ポートフォリオ</a></li>
-					<li><a href="profile.html">プロフィール</a></li>
-					<li><a href="contact.html">お問い合わせ</a></li>
-				</ul>
-			</nav>
+			<?php if ( has_nav_menu( 'global' ) ) : ?>
+				<?php wp_nav_menu( array(
+					'theme_location'  => 'global',
+					'menu_id'         => 'global-menu',
+					'container'       => 'nav',
+					'container_class' => 'global-nav',
+				) ); ?>
+			<?php endif; ?>
 
 		</div>
 	</header>
