c88c5fe46737f47b57bbb159cfbb4b8cfe694592
Kite <ixkaito@gmail.com>
2017-04-14 07:17:25 +0900

    Add indentation

--- index.php
+++ index.php
@@ -54,11 +54,11 @@
 
 	<footer class="page-footer">
 		<div class="footer-widget-area">
-			<ul class="footer-widgets">
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			</ul>
+				<ul class="footer-widgets">
+					<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+					<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+					<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+				</ul>
 			<div class="back-to-top">
 				<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
 			</div>
