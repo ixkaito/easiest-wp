96b9568065c55df9b9d3e39e7df5a9650af6679f
Kite <ixkaito@gmail.com>
2017-04-14 06:58:30 +0900

    Revert is_active_sidebar()

--- index.php
+++ index.php
@@ -69,11 +69,9 @@
 
 		</div>
 
-		<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 			<ul class="side-column">
 				<?php dynamic_sidebar( 'sidebar' ); ?>
 			</ul>
-		<?php endif; ?>
 
 	</div>
 
