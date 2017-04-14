c7ce18779dd892950d82d5fdcb9de6aa86ca5480
Kite <ixkaito@gmail.com>
2017-04-14 12:30:53 +0900

    Change the 404 message

--- 404.php
+++ 404.php
@@ -8,37 +8,7 @@
 			<h1 class="box-heading box-heading-main-col">Not Found</h1>
 			<div class="box-content">
 
-				<?php if ( have_posts() ) : ?>
-
-					<ul class="archive">
-
-						<?php while ( have_posts() ) : ?>
-
-							<?php the_post(); ?>
-
-							<li class="item-archive">
-								<div class="time-and-thumb-archive">
-									<time class="pub-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
-									<?php if ( has_post_thumbnail() ) : ?>
-										<p class="thumb thumb-archive"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'easiestwp-thumbnail' ); ?></a></p>
-									<?php endif; ?>
-								</div>
-								<div class="data-archive">
-									<p class="list-categories-archive"><?php the_category( ', ' ); ?></p>
-									<h2 class="title-archive"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-									<p class="list-tags-archive"><?php the_tags(); ?></p>
-								</div>
-							</li>
-
-						<?php endwhile; ?>
-
-					</ul>
-
-				<?php else : ?>
-
-					<p>投稿がありません。</p>
-
-				<?php endif; ?>
+				<p>お探しのページは見つかりませんでした。</p>
 
 			</div>
 
