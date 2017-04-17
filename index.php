892b770ef4dbeb200517f120ef68d5a7cd023899
Kite <ixkaito@gmail.com>
2017-04-17 11:40:45 +0900

    <?php wp_head(); ?>

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Easiest WP</title>
-	<link rel="stylesheet" href="style.css" />
+	<?php wp_head(); ?>
 </head>
 <body>
 	<header class="page-header">
