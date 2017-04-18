d54db17cf8928c9f826bb80d1a347c24eb2941df
Kite <ixkaito@gmail.com>
2017-04-19 07:21:21 +0900

    If have_comments()

--- comments.php
+++ comments.php
@@ -1,15 +1,17 @@
-<div class="box-generic">
-	<div class="box-content box-comment-display">
-		<h2>コメントとトラックバック</h2>
-		<ul class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'avatar_size' => 130,
-				) );
-			?>
-		</ul>
+<?php if ( have_comments() ) : ?>
+	<div class="box-generic">
+		<div class="box-content box-comment-display">
+			<h2>コメントとトラックバック</h2>
+			<ul class="comment-list">
+				<?php
+					wp_list_comments( array(
+						'avatar_size' => 130,
+					) );
+				?>
+			</ul>
+		</div>
 	</div>
-</div>
+<?php endif; ?>
 <div class="box-generic">
 	<div class="box-content box-comment-input">
 		<h3 id="reply-title" class="comment-reply-title">コメントを残す</h3>
