23d6d67ab25b2308478af8fb6397279abd2811d8
Kite <ixkaito@gmail.com>
2017-04-18 19:44:31 +0900

    Remove add_image_size() for instance

--- functions.php
+++ functions.php
@@ -9,12 +9,6 @@ function easiestwp_setup() {
 
 	add_theme_support( 'post-thumbnails' );
 
-	add_image_size( 'easiestwp-thumbnail', 190, 130, true );
-
-	add_image_size( 'easiestwp-hero', 1200, 630, true );
-
-	add_image_size( 'easiestwp-avatar', 130, 130, true );
-
 	register_nav_menus( array(
 		'global' => 'Global Menu',
 	) );
