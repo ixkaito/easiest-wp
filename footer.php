9293946672533973c35a4015e08e27a30547c66f
Kite <ixkaito@gmail.com>
2017-04-18 13:36:50 +0900

    Add footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+	<footer class="page-footer">
+		<div class="footer-widget-area">
+			<ul class="banner-list">
+				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+			</ul>
+			<div class="back-to-top">
+				<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
+			</div>
+		</div>
+		<div class="copyright">
+			<p>Copyright ©  Gijutsu-Hyohron Co., Ltd.</p>
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>
