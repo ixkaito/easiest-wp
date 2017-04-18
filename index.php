b11049e641bc640e6fd746ab1efa471884959711
Kite <ixkaito@gmail.com>
2017-04-18 13:39:27 +0900

    Add get_header() and get_footer()

--- index.php
+++ index.php
@@ -1,54 +1,58 @@
-<div class="hero"></div>
-<div class="content-area has-side-col">
-	<div class="main-column">
-		<h1 class="box-heading box-heading-main-col">Blog</h1>
-		<div class="box-content">
+<?php get_header(); ?>
 
-			<?php if ( have_posts() ) : ?>
+	<div class="hero"></div>
+	<div class="content-area has-side-col">
+		<div class="main-column">
+			<h1 class="box-heading box-heading-main-col">Blog</h1>
+			<div class="box-content">
 
-				<ul class="archive">
+				<?php if ( have_posts() ) : ?>
 
-					<?php while ( have_posts() ) : ?>
+					<ul class="archive">
 
-					<?php the_post(); ?>
+						<?php while ( have_posts() ) : ?>
 
-						<li class="item-archive">
-							<div class="time-and-thumb-archive">
-								<time class="pub-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
-								<?php if ( has_post_thumbnail() ) : ?>
-									<p class="thumb thumb-archive"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'easiestwp-thumbnail' ); ?></a></p>
-								<?php endif; ?>
-							</div>
-							<div class="data-archive">
-								<p class="list-categories-archive"><?php the_category( ', ' ); ?></p>
-								<h2 class="title-archive"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-								<p class="list-tags-archive"><?php the_tags(); ?></p>
-							</div>
-						</li>
+						<?php the_post(); ?>
 
-					<?php endwhile; ?>
+							<li class="item-archive">
+								<div class="time-and-thumb-archive">
+									<time class="pub-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
+									<?php if ( has_post_thumbnail() ) : ?>
+										<p class="thumb thumb-archive"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'easiestwp-thumbnail' ); ?></a></p>
+									<?php endif; ?>
+								</div>
+								<div class="data-archive">
+									<p class="list-categories-archive"><?php the_category( ', ' ); ?></p>
+									<h2 class="title-archive"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									<p class="list-tags-archive"><?php the_tags(); ?></p>
+								</div>
+							</li>
 
-				</ul>
+						<?php endwhile; ?>
 
-			<?php else : ?>
+					</ul>
 
-				<p>投稿がありません。</p>
+				<?php else : ?>
 
-			<?php endif; ?>
+					<p>投稿がありません。</p>
+
+				<?php endif; ?>
+
+			</div>
+
+			<?php the_posts_pagination( array(
+				'prev_text' => '<img class="arrow" src="' . get_theme_file_uri() . '/images/arrow-left.png" srcset="' . get_theme_file_uri() . '/images/arrow-left@2x.png 2x" alt="前へ">',
+				'next_text' => '<img class="arrow" src="' . get_theme_file_uri() . '/images/arrow-right.png" srcset="' . get_theme_file_uri() . '/images/arrow-right@2x.png 2x" alt="次へ">',
+			) ); ?>
 
 		</div>
 
-		<?php the_posts_pagination( array(
-			'prev_text' => '<img class="arrow" src="' . get_theme_file_uri() . '/images/arrow-left.png" srcset="' . get_theme_file_uri() . '/images/arrow-left@2x.png 2x" alt="前へ">',
-			'next_text' => '<img class="arrow" src="' . get_theme_file_uri() . '/images/arrow-right.png" srcset="' . get_theme_file_uri() . '/images/arrow-right@2x.png 2x" alt="次へ">',
-		) ); ?>
+		<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+			<ul class="side-column">
+				<?php dynamic_sidebar( 'sidebar' ); ?>
+			</ul>
+		<?php endif; ?>
 
 	</div>
 
-	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-		<ul class="side-column">
-			<?php dynamic_sidebar( 'sidebar' ); ?>
-		</ul>
-	<?php endif; ?>
-
-</div>
+<?php get_footer();
