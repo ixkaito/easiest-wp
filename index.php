5c91dfe3022a429211da5f2fc09a5713b6e0a9c8
Kite <ixkaito@gmail.com>
2017-04-18 13:17:19 +0900

    Add is_active_sidebar()

--- index.php
+++ index.php
@@ -73,9 +73,11 @@
 
 		</div>
 
+		<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 			<ul class="side-column">
 				<?php dynamic_sidebar( 'sidebar' ); ?>
 			</ul>
+		<?php endif; ?>
 
 	</div>
 
