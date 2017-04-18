e2720344301c4e850cd8bc5e4e17036e9a0cfe23
Kite <ixkaito@gmail.com>
2017-04-18 17:22:41 +0900

    Improve css for comments

---
+++ comments.php
@@ -0,0 +1,24 @@
+<?php
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<?php if ( have_comments() ) : ?>
+	<div class="box-generic">
+		<div class="box-content box-comment-display">
+			<h2>コメントとトラックバック</h2>
+			<ul class="comment-list">
+				<?php wp_list_comments( array(
+					'avatar_size' => 130,
+				) ); ?>
+			</ul>
+		</div>
+	</div>
+<?php endif; ?>
+
+<div class="box-generic">
+	<div class="box-content box-comment-input">
+		<?php comment_form(); ?>
+	</div>
+</div>
