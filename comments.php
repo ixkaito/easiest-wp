3a27382df3d65f480d1ca67d4009cc3bcaa4af95
Kite <ixkaito@gmail.com>
2017-04-19 07:23:50 +0900

    Use comment_form()

--- comments.php
+++ comments.php
@@ -14,10 +14,6 @@
 <?php endif; ?>
 <div class="box-generic">
 	<div class="box-content box-comment-input">
-		<h3 id="reply-title" class="comment-reply-title">コメントを残す</h3>
-		<p class="comment-notes">メールアドレスが公開されることはありません。 * が付いている欄は必須項目です</p>
-		<form action="#" class="comment-form">
-			<p class="comment-notes"></p>
-		</form>
+		<?php comment_form(); ?>
 	</div>
 </div>
