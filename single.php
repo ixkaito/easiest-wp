8007312a2c00078696315268a5e7a53bdfb1c56a
Kite <ixkaito@gmail.com>
2017-04-19 07:07:01 +0900

    Use comments_template()

--- single.php
+++ single.php
@@ -34,68 +34,9 @@
 				</div>
 			</div>
 
-			<div class="box-generic">
-				<div class="box-content box-comment-display">
-					<h2>コメントとトラックバック</h2>
-					<ul class="comment-list">
-						<li id="comment-1" class="comment even thread-even depth-1 parent">
-							<article id="div-comment-1" class="comment-body">
-								<footer class="comment-meta">
-									<div class="comment-author vcard">
-										<img class="avatar" src="http://placehold.it/130x130">
-										<b class="fn"><a href="#" rel="external nofollow" class="url">WordPress コメントの投稿者</a></b> <span class="says">より:</span>
-									</div><!-- .comment-author -->
-									<div class="comment-metadata">
-										<a href="#">
-											<time datetime="2017-04-16T23:40:30+00:00">2017年4月16日 11:40 PM	</time>
-										</a>
-										<span class="edit-link"><a class="comment-edit-link" href="#">編集</a></span>
-									</div><!-- .comment-metadata -->
-								</footer><!-- .comment-meta -->
-								<div class="comment-content">
-									<p>こんにちは、これはコメントです。<br>
-									コメントの承認、編集、削除を始めるにはダッシュボードの「コメント画面」にアクセスしてください。<br>
-									コメントのアバターは「<a href="https://gravatar.com">Gravatar</a>」から取得されます。</p>
-								</div><!-- .comment-content -->
-								<div class="reply"><a rel="nofollow" class="comment-reply-link" href="#" aria-label="WordPress コメントの投稿者 に返信">返信</a></div>
-							</article><!-- .comment-body -->
-							<ul class="children">
-								<li id="comment-3" class="comment byuser comment-author-admin bypostauthor odd alt depth-2">
-									<article id="div-comment-3" class="comment-body">
-										<footer class="comment-meta">
-											<div class="comment-author vcard">
-												<img class="avatar" src="http://placehold.it/130x130">
-												<b class="fn">admin</b> <span class="says">より:</span>
-											</div><!-- .comment-author -->
-											<div class="comment-metadata">
-												<a href="#">
-													<time datetime="2017-04-18T17:45:33+00:00">2017年4月18日 5:45 PM</time>
-												</a>
-												<span class="edit-link"><a class="comment-edit-link" href="#">編集</a></span>
-											</div><!-- .comment-metadata -->
-										</footer><!-- .comment-meta -->
-										<div class="comment-content">
-											<p>こんにちは、これはコメントです。<br />
-											コメントの承認、編集、削除を始めるにはダッシュボードの「コメント画面」にアクセスしてください。<br />
-											コメントのアバターは「Gravatar」から取得されます。</p>
-										</div><!-- .comment-content -->
-										<div class="reply"><a rel="nofollow" class="comment-reply-link" href="#" aria-label="admin に返信">返信</a></div>
-									</article><!-- .comment-body -->
-								</li><!-- #comment-## -->
-							</ul><!-- .children -->
-						</li><!-- #comment-## -->
-					</ul>
-				</div>
-			</div>
-			<div class="box-generic">
-				<div class="box-content box-comment-input">
-					<h3 id="reply-title" class="comment-reply-title">コメントを残す</h3>
-					<p class="comment-notes">メールアドレスが公開されることはありません。 * が付いている欄は必須項目です</p>
-					<form action="#" class="comment-form">
-						<p class="comment-notes"></p>
-					</form>
-				</div>
-			</div>
+			<?php if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif; ?>
 
 		</div>
 
