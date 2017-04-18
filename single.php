215718f628c6bd0441eee4f199dd5808c0107c35
Kite <ixkaito@gmail.com>
2017-04-18 17:35:47 +0900

    Revert "Improve css for comments"
    This reverts commit e2720344301c4e850cd8bc5e4e17036e9a0cfe23.

--- single.php
+++ single.php
@@ -5,33 +5,119 @@
 	<?php the_post(); ?>
 
 	<div class="hero eyecatch">
-		<?php the_post_thumbnail( 'myfirstwptheme-hero'); ?>
+		<img src="http://placehold.it/1200x630">
 	</div>
 
 	<div class="content-area has-side-col">
 		<div class="main-column">
 			<div class="box-generic">
-				<h1 class="box-heading box-heading-article"><?php the_title(); ?></h1>
+				<h1 class="box-heading box-heading-article">吾輩は猫である。名前はまだ無い。</h1>
 				<div class="box-content">
+
 					<article class="entry">
-						<?php the_content(); ?>
+						<h2>見出しその2</h2>
+						<h3>見出しその3</h3>
+						<h4>見出しその4</h4>
+						<h5>見出しその5</h5>
+						<h6>見出しその6</h6>
+						<p><strong>どこで生れたかとんと見当がつかぬ。</strong>何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。</p>
+						<p><a href="#">しかしその当時は何という考もなかったから別段恐しいとも思わなかった。</a>ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。</p>
+						<p>この書生の掌の裏でしばらくはよい心持に坐っておったが、しばらくすると非常な速力で運転し始めた。書生が動くのか自分だけが動くのか分らないが無暗に眼が廻る。胸が悪くなる。到底助からないと思っていると、どさりと音がして眼から火が出た。それまでは記憶しているがあとは何の事やらいくら考え出そうとしても分らない。</p>
+						<ul>
+							<li>リスト1</li>
+							<li>リスト2</li>
+							<li>リスト3</li>
+							<li>リスト4</li>
+						</ul>
+						<p>ふと気が付いて見ると書生はいない。たくさんおった兄弟が一疋も見えぬ。肝心の母親さえ姿を隠してしまった。その上今までの所とは違って無暗に明るい。眼を明いていられぬくらいだ。はてな何でも容子がおかしいと、のそのそ這い出して見ると非常に痛い。吾輩は藁の上から急に笹原の中へ棄てられたのである。</p>
 					</article>
+
 					<div class="meta-data">
-						<time class="meta meta-entry-date" datetime="<?php echo get_the_date( DATE_W3C ); ?>"><?php echo get_the_date(); ?></time>
-						<p class="meta meta-author"><?php the_author_posts_link(); ?></p>
-						<p class="meta meta-cat"><?php the_category( ', ' ); ?></p>
-						<p class="meta meta-tag"><?php the_tags(); ?></p>
+						<time class="meta meta-entry-date">2016.11.12</time>
+						<p class="meta meta-author">著者名</p>
+						<p class="meta meta-cat">カテゴリ名</p>
+						<p class="meta meta-tag">タグ, タグ, タグ, タグ</p>
 					</div>
+
 					<div class="related-posts">
 						<h2>関連する記事</h2>
-						<?php the_post_navigation(); ?>
+						<ul class="archive">
+							<li class="item-archive">
+								<div class="time-and-thumb-archive">
+									<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+									<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
+								</div>
+								<div class="data-archive">
+									<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
+									<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
+									<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
+								</div>
+							</li>
+							<li class="item-archive">
+								<div class="time-and-thumb-archive">
+									<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+									<p class="thumb thumb-archive"><a href="single.html"><img src="http://placehold.it/190x130"></a></p>
+								</div>
+								<div class="data-archive">
+									<p class="list-categories-archive"><a href="archive.html">カテゴリ名</a></p>
+									<h2 class="title-archive"><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></h2>
+									<p class="list-tags-archive"><a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a>, <a href="archive.html">タグ</a></p>
+								</div>
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
+			<div class="box-generic">
+				<div class="box-content box-comment-display">
+					<h2>コメントとトラックバック</h2>
+					<ul class="comment-list">
 
-			<?php if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif; ?>
+						<li class="item-comment comment depth-1 parent">
+							<div class="time-and-thumb-comment">
+								<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+								<p class="thumb thumb-comment"><img src="http://placehold.it/130x130"></p>
+							</div>
+							<div class="data-comment">
+								<div class="comment-author">WordPressコメントの投稿者</div>
+								<div class="comment-body">
+									<p>こんにちは、これはコメントです。</p>
+								</div>
+								<div class="reply"><a href="#">返信</a></div>
+							</div>
+							<ul class="children">
+								<li class="item-comment comment depth-1 parent">
+									<div class="time-and-thumb-comment">
+										<time class="pub-date" datetime="2011-12-03">Sep 8, 2016</time>
+										<p class="thumb thumb-comment"><img src="http://placehold.it/130x130"></p>
+									</div>
+									<div class="data-comment">
+										<div class="comment-author">WordPressコメントの投稿者</div>
+										<div class="comment-body">
+											<p>こんにちは、これはコメントです。<br />
+											コメントの承認、編集、削除を始めるにはダッシュボードの「コメント画面」にアクセスしてください。<br />
+											コメントのアバターは「Gravatar」から取得されます。</p>
+										</div>
+										<div class="reply"><a href="#">返信</a></div>
+									</div>
+									</ul>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="box-generic">
+				<div class="box-content box-comment-input">
+					<h2>コメントをどうぞ</h2>
+					<p>メールアドレスが公開されることはありません。</p>
+					<form action="#" class="comment-form">
+						<p class="comment-notes">
+
+						</p>
+					</form>
+				</div>
+			</div>
 
 		</div>
 
@@ -41,4 +127,4 @@
 
 <?php endwhile; ?>
 
-<?php get_footer(); ?>
+<?php get_footer();
