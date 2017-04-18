101b07fafe083bfcaa739c545ce3a714a1bc631e
Kite <ixkaito@gmail.com>
2017-04-18 13:42:10 +0900

    Include header.php

--- single.php
+++ single.php
@@ -1,30 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Easiest WP</title>
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<header class="page-header">
-		<div class="header-area">
-			<div class="panel-site-title">
-				<p class="site-title"><a href="index.html">Easiest WP</a></p>
-				<p class="site-subtitle">Just another WordPress site</p>
-			</div>
-
-			<nav class="global-nav">
-				<ul id="global-menu" class="menu">
-					<li class="current-menu-item"><a href="index.html">ホーム</a></li>
-					<li><a href="portfolio.html">ポートフォリオ</a></li>
-					<li><a href="profile.html">プロフィール</a></li>
-					<li><a href="contact.html">お問い合わせ</a></li>
-				</ul>
-			</nav>
-
-		</div>
-	</header>
+<?php get_header(); ?>
 
 	<div class="hero eyecatch">
 		<img src="http://placehold.it/1200x630">
