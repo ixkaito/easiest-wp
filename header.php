841d369a6edf22cb50706ab7c4a60ca58e16c585
Kite <ixkaito@gmail.com>
2017-04-18 13:31:29 +0900

    Add header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<header class="page-header">
+		<div class="header-area">
+			<div class="panel-site-title">
+				<p class="site-title"><a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-subtitle"><?php bloginfo( 'description' ); ?></p>
+			</div>
+
+			<?php if ( has_nav_menu( 'global' ) ) : ?>
+				<?php wp_nav_menu( array(
+					'theme_location'  => 'global',
+					'menu_id'         => 'global-menu',
+					'container'       => 'nav',
+					'container_class' => 'global-nav',
+				) ); ?>
+			<?php endif; ?>
+
+		</div>
+	</header>
