881d391b9af6d4a378309ffe2ab61e3d3cd711a1
Kite <ixkaito@gmail.com>
2017-04-18 11:19:13 +0900

    Use get_theme_file_uri()

--- index.php
+++ index.php
@@ -65,8 +65,8 @@
 			</div>
 
 			<?php the_posts_pagination( array(
-				'prev_text' => '<img class="arrow" src="images/arrow-left.png" srcset="images/arrow-left@2x.png 2x" alt="前へ">',
-				'next_text' => '<img class="arrow" src="images/arrow-right.png" srcset="images/arrow-right@2x.png 2x" alt="次へ">',
+				'prev_text' => '<img class="arrow" src="' . get_theme_file_uri() . '/images/arrow-left.png" srcset="' . get_theme_file_uri() . '/images/arrow-left@2x.png 2x" alt="前へ">',
+				'next_text' => '<img class="arrow" src="' . get_theme_file_uri() . '/images/arrow-right.png" srcset="' . get_theme_file_uri() . '/images/arrow-right@2x.png 2x" alt="次へ">',
 			) ); ?>
 
 		</div>
