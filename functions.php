473fe94b72b6da6c263a7ef87400ba204f295213
Kite <ixkaito@gmail.com>
2017-04-18 19:46:41 +0900

    add_image_size()

--- functions.php
+++ functions.php
@@ -8,6 +8,10 @@ function easiestwp_setup() {
 	add_theme_support( 'title-tag' );
 
 	add_theme_support( 'post-thumbnails' );
+
+	add_image_size( 'easiestwp-thumbnail', 190, 130, true );
+
+	add_image_size( 'easiestwp-hero', 1200, 630, true );
 }
 add_action( 'after_setup_theme', 'easiestwp_setup' );
 
