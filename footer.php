57b529b666f71198011237a31d4d688375d612c6
Kite <ixkaito@gmail.com>
2017-04-19 16:20:40 +0900

    Show arrow-up images

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 				</ul>
 			<?php endif; ?>
 			<div class="back-to-top">
-				<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
+				<a href="#"><img src="<?php echo esc_url( get_theme_file_uri() ); ?>/images/arrow-up.png" srcset="<?php echo esc_url( get_theme_file_uri() ); ?>/images/arrow-up@2x.png 2x" alt="">TOP</a>
 			</div>
 		</div>
 		<div class="copyright">
