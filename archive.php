e35f64ca6852259efb1855065640b50353b809e9
Kite <ixkaito@gmail.com>
2017-04-19 11:42:03 +0900

    Revert archive.php

--- archive.php
+++ archive.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 	<div class="page-title">
-		<h1>「<?php the_search_query(); ?>」の検索結果</h1>
+		<h1><?php the_archive_title(); ?></h1>
 	</div>
 
 	<div class="content-area has-side-col">
 		<div class="main-column">
-			<h1 class="box-heading box-heading-main-col">「<?php the_search_query(); ?>」の検索結果</h1>
+			<h1 class="box-heading box-heading-main-col"><?php the_archive_title(); ?></h1>
 			<div class="box-content">
 
 				<?php if ( have_posts() ) : ?>
@@ -37,7 +37,7 @@
 
 				<?php else : ?>
 
-					<p>何も見つかりませんでした。</p>
+					<p>投稿がありません。</p>
 
 				<?php endif; ?>
 
