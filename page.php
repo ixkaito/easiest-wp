213c31019645d3f0be5aa0677d46ff3b33ddba5d
Kite <ixkaito@gmail.com>
2017-04-19 10:31:13 +0900

    Get footer in page.php

--- page.php
+++ page.php
@@ -34,20 +34,4 @@
 		</div>
 	</div>
 
-	<footer class="page-footer">
-		<div class="footer-widget-area">
-			<ul class="banner-list">
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
-			</ul>
-			<div class="back-to-top">
-				<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
-			</div>
-		</div>
-		<div class="copyright">
-			<p>Copyright ©  Gijutsu-Hyohron Co., Ltd.</p>
-		</div>
-	</footer>
-</body>
-</html>
+<?php get_footer();
